e5c878936b8d3bafcca73a4920e754357feea3a1
rickabraham <124325080+rickabraham@users.noreply.github.com>
2024-02-01 12:56:54 -0600

    Update Acronyms.py

--- Acronyms.py
+++ Acronyms.py
@@ -1,19 +1,19 @@
 def getAcronym(name):
-    acronym = name[0]
+    acronym = name[0]  # Start the acronym with the first character of the name
     
-    # Count the number of times the character occurs in the word
+    # Count the number of spaces in the name to determine the number of words
     num_occurrences = name.count(" ")
 
-    # Build the aconym.
+    # Initialize the position to start searching for the next word
     position = 1
     for occurrence in range(num_occurrences):
-        position = name.find(" ", position) # Find the position of the next occurrence
-        position = position + 1
-        if name[position].isupper():
-            acronym = acronym + name[position]
+        position = name.find(" ", position)  # Find the position of the next space
+        position += 1  # Move to the character after the space
+        if position < len(name) and name[position].isupper():  # Check if the next character is uppercase and within the string bounds
+            acronym += name[position]  # Add the uppercase character to the acronym
     
     return acronym
-    
 
-phrase = input()
-print(getAcronym(phrase))
+# Example usage
+phrase = input("Enter a phrase: ")
+print("The acronym is:", getAcronym(phrase))
